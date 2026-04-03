2c2e8218117bd852a1b8d2c332e77fd265fcd8e6
xLegarda <80797289+xLegarda@users.noreply.github.com>
2026-04-03 18:00:11 -0500

    correcion

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,16 +1,12 @@
 import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 
-const isProduction = process.env.NODE_ENV === "production";
-
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
   datasource: {
-    url: isProduction
-      ? `${env("TURSO_DATABASE_URL")}?authToken=${env("TURSO_AUTH_TOKEN")}`
-      : env("DATABASE_URL"),
+    url: env("DATABASE_URL")?? "",
   },
 });
